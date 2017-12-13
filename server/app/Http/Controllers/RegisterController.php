691f7c13426068c48e527908d966aa8a9acde341
root <root@ubuntu.8.8.8>
2017-12-12 21:24:42 -0500

    fix validate

--- server/app/Http/Controllers/RegisterController.php
+++ server/app/Http/Controllers/RegisterController.php
@@ -19,9 +19,8 @@ class RegisterController extends Controller
         $this->validate($request, [
             'name'=>'required',
             'game_id'=>'required',
-            'email'=>'required|email|min:6|max:100',
-            'password'=>'required|min:6|confirmed',
-            'password_confirmation'=>"required"
+            'email'=>'required|email|min:6|max:100|unique:users,email',
+            'password'=>'required|min:6'
         ]);
 
         return response()->jsonOk($this->userService->registerUser($request));
